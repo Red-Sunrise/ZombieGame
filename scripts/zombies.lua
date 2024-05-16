4d03b1ab92f08c3c26cd262e09d6c2a8d92929b8
Yaroslaw Proskurin <proskurin_yaroslaw@mail.ru>
2024-05-16 13:24:29 +0300

    Added preset zombie types in factory
    Added preset zombie types (weak, medium, strong) with different characteristics. The zombie factory now creates zombies of these types instead of generating random characteristics

---
+++ scripts/zombies.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.zombies = {
+	weak = {
+		hp = 50,
+		damage = 10,
+		velocity = 200
+	},
+	medium = {
+		hp = 100,
+		damage = 20,
+		velocity = 150
+	},
+	weak = {
+		hp = 200,
+		damage = 30,
+		velocity = 100
+	}
+}
+
+return M
